cbc40428dd729f89cb902365f54de25f7c332333
HappyCodingMama <happycodingmama@gmail.com>
2020-10-28 23:12:12 +0900

    image slider with bg

--- Image slider - background/app.js
+++ Image slider - background/app.js
@@ -1,25 +1,77 @@
 const slider = document.querySelector('.slider');
-const buttons = document.querySelector('.btn');
-const options = document.querySelector('.option');
-const slides = document.querySelector('.img');
+const buttons = document.querySelectorAll('.btn');
+const options = document.querySelectorAll('.option');
+const slides = document.querySelectorAll('.img');
+const backgrounds = document.querySelectorAll('.bg');
 
 var index = 1;
+var op_index = 0;
 var size = slides[index].clientWidth;
 
 update();
 
 function update() {
-      slider.getElementsByClassName.transform = "translateX(" + (-size * index) + "px)";
+      slider.style.transform = "translateX(" + (-size * index) + "px)";
+
+      options.forEach(op => op.classList.remove('colored'));
+      options[op_index].classList.add('colored');
+
+      backgrounds.forEach(bg => bg.classList.remove('show'));
+      backgrounds[op_index].classList.add('show');
+}
+
+function slide() {
+      slider.style.transition = "transform .5s ease-in-out";
+      update();
 }
 
 function btnCheck() {
       if (this.id === "prev") {
             index--;
+
+            if (op_index == 0) {
+                  op_index = 4;
+            } else {
+                  op_index--;
+            }
       }
 
       else if (this.id === "next") {
             index++;
+
+
+            if (op_index == 4) {
+                  op_index = 0;
+            } else {
+                  op_index++;
+            }
       }
+
+      slide();
 }
 
+function optionFunc() {
+      let i = Number(this.getAttribute('option-index'));
+      op_index = i;
+      index = i + 1;
+
+      slide();
+}
+
+
+slider.addEventListener('transitionend', () => {
+      if (slides[index].id === "first") {
+            slider.style.transition = "none";
+            index = slides.length - 2;
+            slider.style.transform = "translateX(" + (-size * index) + "px)";
+      }
+
+      else if (slides[index].id === "last") {
+            slider.style.transition = "none";
+            index = 1;
+            slider.style.transform = "translateX(" + (-size * index) + "px)";
+      }
+})
+
 buttons.forEach(btn => btn.addEventListener('click', btnCheck));
+options.forEach(option => option.addEventListener('click', optionFunc));
